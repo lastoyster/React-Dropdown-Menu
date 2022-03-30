6d724ca733ac0723b1f86a7f9324b79452e097ab
Pratiksha <95347099+lastoyster@users.noreply.github.com>
2022-03-30 12:48:05 +0530

    Add files via upload

---
+++ src/menuItems.js
@@ -0,0 +1,27 @@
+export const menuItems = [
+  {
+    title: "Home"
+  },
+  {
+    title: "Services",
+    submenu: [
+      {
+        title: "Web Design"
+      },
+      {
+        title: "Web Development"
+      },
+      {
+        title: "SEO"
+      }
+    ]
+  },
+  {
+    title: "About",
+    submenu: [
+      {
+        title: "Who We Are"
+      }
+    ]
+  }
+];
